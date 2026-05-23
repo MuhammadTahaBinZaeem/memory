dcfe9c8622b5ba747a9e9801c953cf91439a3425
Taha Zaeem <tahabinzaeem0@gmail.com>
2026-05-23 19:09:43 +0500

    Add proteusgen package init

---
+++ src/proteusgen/__init__.py
@@ -0,0 +1,12 @@
+"""Proteus project generation research utilities.
+
+This package stores early helper code for analyzing, normalizing, and
+repacking Proteus `.pdsprj` project containers created by the user.
+"""
+
+__all__ = [
+    "pdsprj",
+    "versioning",
+    "inspectors",
+    "reports",
+]
